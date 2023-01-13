019f611dd7125a943dbb1e8893a145b2793c447a
Hasibix Hasib <hasibix3690@gmail.com>
2023-01-14 00:57:29 +0600

    First commit

---
+++ src/main/java/net/hasibix/hasicraft/discordbot/Main.java
@@ -0,0 +1,18 @@
+package net.hasibix.hasicraft.discordbot;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+
+    public static void main(String[] args) throws LoginException {
+
+        JDA bot = JDABuilder.createDefault(System.getenv("BOT_TOKEN"))
+                .setActivity(Activity.playing("hasi help | Play.HasiCraft.net"))
+                .build();
+
+    }
+}
