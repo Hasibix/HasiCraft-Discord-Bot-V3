bcd08843604c76e22fb3a364d8c9fe46b821d443
Hasibix <hasibix3690@gmail.com>
2023-01-14 15:32:24 +0000

    Nothing has changed yet.

--- src/main/java/net/hasibix/hasicraft/discordbot/Main.java
+++ src/main/java/net/hasibix/hasicraft/discordbot/Main.java
@@ -9,8 +9,10 @@ import javax.security.auth.login.LoginException;
 public class Main {
 
     public static void main(String[] args) throws LoginException {
+        
         JDA bot = JDABuilder.createDefault(System.getenv("BOT_TOKEN"))
                 .setActivity(Activity.playing("hasi help | Play.HasiCraft.net"))
                 .build();
+
     }
 }
