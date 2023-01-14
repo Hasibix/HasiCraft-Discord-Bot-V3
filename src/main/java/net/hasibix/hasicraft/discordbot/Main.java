5c27404b39fda499d57a751edcb3105ab14ed87a
Hasibix Hasib <hasibix3690@gmail.com>
2023-01-14 22:04:10 +0600

    DotEnv

--- src/main/java/net/hasibix/hasicraft/discordbot/Main.java
+++ src/main/java/net/hasibix/hasicraft/discordbot/Main.java
@@ -3,14 +3,16 @@ package net.hasibix.hasicraft.discordbot;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 
 import javax.security.auth.login.LoginException;
 
 public class Main {
 
     public static void main(String[] args) throws LoginException {
-        
-        JDA bot = JDABuilder.createDefault(System.getenv("BOT_TOKEN"))
+        Dotenv dotenv = Dotenv.configure().load();
+        JDA bot = JDABuilder.createDefault(dotenv.get("BOT_TOKEN"))
                 .setActivity(Activity.listening("hasi help | Play.HasiCraft.net"))
                 .build();
 
