721bff6f08644b9049fdfd2561d25c650ea99ad4
Hasibix <hasibix3690@gmail.com>
2023-01-19 16:23:54 +0000

    e

--- src/main/java/net/hasibix/hasicraft/discordbot/commands/info/Ping.java
+++ src/main/java/net/hasibix/hasicraft/discordbot/commands/info/Ping.java
@@ -1,8 +1,6 @@
 package net.hasibix.hasicraft.discordbot.commands.info;
 
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.hasibix.hasicraft.discordbot.models.client.Command;
 import net.hasibix.hasicraft.discordbot.models.client.HasiBot;
@@ -16,8 +14,12 @@ public class Ping {
             new Permission[]{},
             "Info",
             new OptionData[]{},
-            (jda, event, args) -> event.getMessage().reply("Pong!"),
-            (jda, event, args) -> event.reply("Pong!")
+            (jda, event, args) -> {
+                event.getMessage().reply("Pong!");
+            },
+            (jda, event, args) -> {
+                event.reply("Pong!");
+            }
         );
         
         HasiBot.commandHandler.addCommand(command);
