f3cb08e9feca6ae3dfe2efc63b91bd8049e9af9f
Hasibix <hasibix3690@gmail.com>
2023-01-20 11:54:44 +0000

    e

--- src/main/java/net/hasibix/hasicraft/discordbot/commands/info/Ping.java
+++ src/main/java/net/hasibix/hasicraft/discordbot/commands/info/Ping.java
@@ -1,9 +1,6 @@
 package net.hasibix.hasicraft.discordbot.commands.info;
 
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.hasibix.hasicraft.discordbot.models.client.Command;
 import net.hasibix.hasicraft.discordbot.models.client.HasiBot;
@@ -18,17 +15,13 @@ public class Ping {
             "Info",
             new OptionData[]{},
             (client, event, args) -> {
-                event.getMessage().reply("Pong!");
+                event.getMessage().reply("Pong!").queue();
             },
             (client, event, args) -> {
-                event.reply("Pong!");
+                event.reply("Pong!").queue();
             }
         );
         
         HasiBot.commandHandler.addCommand(command);
-    }
-    
-    public static void slashrun(JDA client, SlashCommandInteractionEvent event, OptionMapping[] args) {
-        event.reply("Pong!");
     }
 }
